8b0e4e8f34ba444ccdacc861f69e5f8600a73c5f
colee222 <nfsulli@gmail.com>
2021-11-23 21:22:09 -0800

    Final

--- storytime/settings.py
+++ storytime/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1&mm)8(-h#%13vs0o3ilnfsw)uz)t1wc)(kq8d-)c1wkg@c=j9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog.apps.BlogConfig',
+    'ourPage',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
